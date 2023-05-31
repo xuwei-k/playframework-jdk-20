94a0fad963e120569dedba543ae97a55d58a52d8
Matthias Kurz <m.kurz@irregular.at>
2023-05-31 09:22:02 +0200

    Revert "override SSLEngineProvider"
    This reverts commit 743567f16e4552f2b8b3706982c00436981d9d18.

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -2,12 +2,8 @@ package controllers
 
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
-import play.api.{Application, Configuration}
 import play.api.test._
 import play.api.test.Helpers._
-import play.server.api.SSLEngineProvider
-import javax.net.ssl.SSLContext
-import javax.net.ssl.SSLEngine
 
 class HomeControllerSpec extends PlaySpec with GuiceOneServerPerSuite {
 
@@ -19,21 +15,4 @@ class HomeControllerSpec extends PlaySpec with GuiceOneServerPerSuite {
       }
     }
   }
-
-  private[this] val factory = new DefaultTestServerFactory {
-    override def overrideServerConfiguration(app: Application): Configuration =
-      Configuration("play.server.https.engineProvider" -> classOf[MySSLEngineProvider].getName)
-  }
-
-  override protected final implicit lazy val runningServer: RunningServer =
-    factory.start(app)
-}
-
-
-class MySSLEngineProvider extends SSLEngineProvider {
-  override def createSSLEngine(): SSLEngine =
-    sslContext().createSSLEngine()
-
-  override def sslContext(): SSLContext =
-    SSLContext.getDefault
 }
